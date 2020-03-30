1765a0e2a38a7b56be97f88bfb5ed182d2be4545
GalaxyHiTech <jouharsoufiane00@gmail.com>
2020-03-30 22:39:19 +0100

    initial commit

---
+++ src/test/java/com/ms/currencyexchange/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
